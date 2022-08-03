f93a2262cfd8e1fa80196ff42d6e6fe7f6699fc0
Sangpil <tankteam20@gmail.com>
2022-08-03 23:46:04 +1000

    fetch data by input query from user

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,22 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import Aos from "aos";
 import Header from "./components/header";
 import Videos from "./components/videos";
+import "aos/dist/aos.css";
+const API_KEY = "AIzaSyAKpZ71aenK6nuDcMIu07KIm4Y9CQ5FCD4";
 
 function App() {
+    const [search, setSearch] = useState("");
     const [videos, setVideos] = useState([]);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
+        Aos.init();
         const getYoutubeAsync = async () => {
             const response = await fetch(
-                "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAKpZ71aenK6nuDcMIu07KIm4Y9CQ5FCD4",
+                // "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAKpZ71aenK6nuDcMIu07KIm4Y9CQ5FCD4",
+                `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${search}&key=${API_KEY}`,
                 {
                     method: "GET",
                     headers: { "Content-Type": "application/json" },
@@ -19,22 +25,28 @@ function App() {
             const data = await response.json();
             setVideos(data.items);
             setLoading(false);
-
             return data;
         };
         getYoutubeAsync().then((data) => {
             console.log(data);
         });
-    }, []);
+    }, [search]);
 
     const onSearch = (detail) => {
         console.log(detail);
+        setSearch(detail);
     };
 
     return (
         <div className='App'>
             <Header handleSearch={onSearch} />
-            {!loading ? <Videos key={videos.id} data={videos} /> : "loading"}
+            <div data-aos='slide-up' data-aos-offset='400'>
+                {!loading ? (
+                    <Videos key={videos.id} data={videos} />
+                ) : (
+                    "loading"
+                )}
+            </div>
         </div>
     );
 }
--- src/components/video.jsx
+++ src/components/video.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import styles from "./video.module.css";
-import img1 from "../imgs/testing.png";
 
 const Video = (props) => {
     const thumbnail = props.data;
+
     return (
         <>
             {thumbnail.map((item) => {
                 return (
-                    <div key={item.id} className={styles.videoCard}>
+                    <div key={item.id.videoId} className={styles.videoCard}>
                         <div className={styles.image}>
                             <img
                                 src={item.snippet.thumbnails.medium.url}
